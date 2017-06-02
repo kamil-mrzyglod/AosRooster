5069f9bd348b17beff862952ee3ccf3c7c14d7db
Kamil Mrzygłód <kamz@oticon.com>
2017-06-02 13:52:32 +0200

    #2 - added a method, which detects encoding of a file so it should cover most cases when special chars are in a file

--- Rooster/MainWindow.cs
+++ Rooster/MainWindow.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Rooster
@@ -30,7 +31,7 @@ namespace Rooster
         {
             if (openRoosterDialog.ShowDialog() == DialogResult.OK)
             {
-                var content = File.ReadAllText(openRoosterDialog.FileName);
+                var content = File.ReadAllText(openRoosterDialog.FileName, GetEncoding(openRoosterDialog.FileName));
                 ReadRoosterFile(content);
                 EnableAddResultButton();
             }
@@ -225,6 +226,31 @@ namespace Rooster
                     });
                 }
             }
+        }
+
+        /// <summary>
+        /// Determines a text file's encoding by analyzing its byte order mark (BOM).
+        /// Defaults to ASCII when detection of the text file's endianness fails.
+        /// </summary>
+        /// <param name="filename">The text file to analyze.</param>
+        /// <returns>The detected encoding.</returns>
+        /// Taken from https://stackoverflow.com/questions/3825390/effective-way-to-find-any-files-encoding
+        public static Encoding GetEncoding(string filename)
+        {
+            // Read the BOM
+            var bom = new byte[4];
+            using (var file = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                file.Read(bom, 0, 4);
+            }
+
+            // Analyze the BOM
+            if (bom[0] == 0x2b && bom[1] == 0x2f && bom[2] == 0x76) return Encoding.UTF7;
+            if (bom[0] == 0xef && bom[1] == 0xbb && bom[2] == 0xbf) return Encoding.UTF8;
+            if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
+            if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
+            if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return Encoding.UTF32;
+            return Encoding.Default;
         }
     }
 }
